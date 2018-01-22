f34ebdf0caf42e4fa0a4c7daa0bc5248fefd6977
Tuan Anh Le <anhlt1983@gmail.com>
2018-01-22 22:28:39 +0900

    continous monitoring

--- public/scripts/geo.js
+++ public/scripts/geo.js
@@ -1,12 +1,55 @@
+var watchId = 0;
+
 $(document).ready(function () {
     getLocation();
+    $('#startMonitoring').on('click', getLocation);
+    $('#stopMonitoring').on('click', endWatch);
 });
 
+/*
 function getLocation() {
     if (supportsGeoLocation()) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else {
         showMessage("Getlocation is not supported by this browser");
+    }
+}
+*/
+
+// continous monitoring position
+// callback when position is updated
+function getLocation() {
+    if (supportsGeoLocation()) {
+        var options = {
+            enableHighAccuracy: true
+        };
+        watchId = navigator.geolocation.watchPosition(showPosition, showError, options);
+    }
+    else {
+        showMessage("Geolocation is not supported by this browser.");
+    }
+}
+
+function getDistance(lat1, lon1, lat2, lon2) {    
+    var earthRadius = 3959; //miles
+    var latRadians = getRadians(lat2 - lat1);
+    var lonRadians = getRadians(lon2 - lon1);
+    
+    var a = Math.sin(latRadians / 2) * Math.sin(latRadians / 2) +
+            Math.cos(getRadians(lat1)) * Math.cos(getRadians(lat2)) *
+            Math.sin(lonRadians / 2) * Math.sin(lonRadians / 2);
+    
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var distance = earthRadius * c;
+
+    return distance;
+}
+
+function endWatch() {
+    if (watchId != 0) {
+        navigator.geolocation.clearWatch(watchId);
+        watchId = 0;
+        showMessage("Monitoring ended");
     }
 }
 
